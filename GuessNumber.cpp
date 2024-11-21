467fb3ab9491584e7005045858490d69a2c31d1b
Maria <lebedeva.maria@stud.onu.edu.ua>
2024-11-21 13:54:32 +0200

    Add files via upload

--- GuessNumber.cpp
+++ GuessNumber.cpp
@@ -1,11 +1,7 @@
 ﻿#include <iostream>
 #include <ctime>
 #include <Windows.h>
-
-using namespace std;
-
 int generateRandomNumber(int& min, int& max);
-
 void binarySearch(int& target, int low, int high, int& attempts);
 
 int main() {
@@ -16,37 +12,32 @@ int main() {
     int max = 1000;
     int targetNumber = generateRandomNumber(min, max);
     int attempts = 0;
-
-    cout << "Задуманное число: " << targetNumber << endl; 
+    std::cout << "Задуманное число: " << targetNumber << std::endl; 
     binarySearch(targetNumber, min, max, attempts);
-
     return 0;
 }
 
 int generateRandomNumber(int& min, int& max) {
     return rand() % (max - min + 1) + min;
 }
-
 void binarySearch(int& target, int low, int high, int& attempts) {
     if (low > high) {
-        cout << "Число не найдено в диапазоне." << endl;
+        std::cout << "Число не найдено в диапазоне." << std::endl;
         return;
     }
-
     int guess = (low + high) / 2;
     attempts++;
-
-    cout << "Попытка #" << attempts << ": " << guess << endl;
+    std::cout << "Попытка #" << attempts << ": " << guess << std::endl;
 
     if (guess < target) {
-        cout << "Число больше, чем " << guess << endl;
+        std::cout << "Число больше, чем " << guess << std::endl;
         binarySearch(target, guess + 1, high, attempts);
     }
     else if (guess > target) {
-        cout << "Число меньше, чем " << guess << endl;
+        std::cout << "Число меньше, чем " << guess << std::endl;
         binarySearch(target, low, guess - 1, attempts);
     }
     else {
-        cout << "Число угадано! Это " << guess << " после " << attempts << " попыток." << endl;
+        std::cout << "Число угадано! Это " << guess << " после " << attempts << " попыток." << std::endl;
     }
 }
